b5f6ce94f6c141b9c8d651bebddc3cffbdb79002
Aljith KJ <kwdeathlok@gmail.com>
2023-02-05 19:38:01 +0530

    updated

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ const productRouter = require('./Routes/product.router');
 const authRouter = require('./Routes/auth.router'); 
 const cors = require('cors')
 const passport = require('passport');
-// require('./controller/google.authenticate');
+require('./controller/google.authenticate');
 const session = require('express-session');
-
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const GoogleUser = require('./models/googleUser.model');
 
 const app = express();
 app.use(cors());
@@ -48,29 +45,6 @@ app.get('/', (req, res)=>{
     res.send(process.env.PORT); 
 })
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://firstcry-ix8k.onrender.com/google/callback",
-    passReqToCallbaack: true
-  },
-  async function(accessToken, refreshToken, profile, done) {
-      let userObj = {
-            name: profile.displayName,
-            email: profile.emails[0].value,
-            login_type: 'google'
-      }
-      await GoogleUser.create(userObj);
-      return done(null, profile);   
-  }    
-));
-passport.serializeUser((user, done)=> {
-    done(null, user);
-})
-
-passport.deserializeUser((user, done)=> {
-    done(null, user);
-})
 
 
 async function startServer(){
