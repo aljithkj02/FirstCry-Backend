80a776e078b15369926a1cc9bea5f260a8e949ac
Aljith KJ <kwdeathlok@gmail.com>
2023-02-05 17:44:04 +0530

    updated

--- controller/google.authenticate.js
+++ controller/google.authenticate.js
@@ -5,7 +5,7 @@ const GoogleUser = require('../models/googleUser.model');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/google/callback",
+    callbackURL: "https://firstcry-ix8k.onrender.com/google/callback",
     passReqToCallbaack: true
   },
   async function(accessToken, refreshToken, profile, done) {
